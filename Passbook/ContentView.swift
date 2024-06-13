f96210e362cb76caf102dbd0da8b4ca2c1412b3d
Maxime Savehilaghi <msavehilaghi99@gmail.com>
2024-06-13 14:16:14 -0400

    Initial Commit

---
+++ Passbook/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Passbook
+//
+//  Created by Maxime Savehilaghi on 2024-06-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Passbook/PassbookApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassbookApp.swift
+//  Passbook
+//
+//  Created by Maxime Savehilaghi on 2024-06-13.
+//
+
+import SwiftUI
+
+@main
+struct PassbookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
